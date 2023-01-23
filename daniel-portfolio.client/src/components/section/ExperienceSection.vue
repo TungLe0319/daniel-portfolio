5c310912863b312fdc3c8461dcadca367e7ecdd1
daniel-le97 <daniel97.le@gmail.com>
2023-01-23 12:30:19 -0700

    fix codeworks logo

--- daniel-portfolio.client/src/components/section/ExperienceSection.vue
+++ daniel-portfolio.client/src/components/section/ExperienceSection.vue
@@ -35,10 +35,10 @@
               /></div
           >
          -->
-        <div class="card-1">
+        <div class="">
 
-  <img src="https://avatars.githubusercontent.com/u/94322600?s=280&v=4" alt="Front image" class="front-image">
-  <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fbcw.blob.core.windows.net%2Fpublic%2Fimg%2F8600856373152463&f=1&nofb=1&ipt=7556aa255fdb3f0a77caa1be29b0f23e5f3d403a76f098fca104b1911e9c485b&ipo=images" alt="Back image" class="back-image" width="175" height="175">
+  <!-- <img src="https://avatars.githubusercontent.com/u/94322600?s=280&v=4" alt="Front image" class="front-image"> -->
+  <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fbcw.blob.core.windows.net%2Fpublic%2Fimg%2F8600856373152463&f=1&nofb=1&ipt=7556aa255fdb3f0a77caa1be29b0f23e5f3d403a76f098fca104b1911e9c485b&ipo=images" alt="Back image" class="back-image m-5" width="175" height="175">
 </div>
 
         </div>
